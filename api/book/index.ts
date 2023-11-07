dac7747a7e83918e4ed93fd819e59a1aaa40b1c0
Victor Manuel Lagunas Franco <victormanuelfrancodev@gmail.com>
2023-11-07 17:33:30 +0900

    express
    ex

--- api/book/index.ts
+++
@@ -1,9 +0,0 @@
-import Database from 'bun:sqlite'
-import { CromoHandler } from 'cromo'
-
-export const GET: CromoHandler = ({ responseInit }) => {
-  const db = new Database('./src/database/bible.db')
-  const query = db.query('SELECT id, abbreviation, name, nameLong FROM book')
-
-  return Response.json(query.all(), responseInit)
-}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-import { Cromo } from 'cromo'
-import { cors } from './middleware/cors'
+import express, { json } from 'express'
+import routes from './routes'
+import compression from 'compression'
+import helmet from 'helmet'
+import cors from 'cors'
+import { errorHandler } from './error-handler'
 
-const cromo = new Cromo({
-  dir: './api',
-  port: 3000,
-})
+// Instancia de express
+const app = express()
+const port = process.env.PORT || 5000
 
-cromo.setMiddleware([cors])
-cromo.start(port => {
-  console.log(`Server running on port ${port}`)
+app.use(helmet())
+app.use(cors())
+app.use(compression())
+app.use(json())
+// app.use(catchErrors)
+
+app.use('/', routes)
+
+app.use(errorHandler)
+
+app.listen(port, () => {
+  console.log(`Servidor ejecutandose en el puerto ${port}`)
 })
